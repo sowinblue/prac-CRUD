2c65edc08a527ecadb20a1e6a0a3f5e7895983dd
sowinblue <249227634+sowinblue@users.noreply.github.com>
2025-12-24 17:46:09 +0900

    delete 함수 정의

--- main.py
+++ main.py
@@ -13,7 +13,16 @@ def addcreate_m(addtext):
         print("메세지 추가")
 
 
+def delete_m(deletetext):
+    with open("to_minamkong.txt","w",encoding="utf-9") as f:
+        f.write(" ")
+        print("메세지 삭제")
+
+
 newcreate_m("안녕하세요 강사님")
 addcreate_m("\n근데 제가 드릴 말씀이 있습니다")
 addcreate_m("\n...")
 addcreate_m("\n여기까지 하겠습니다.")
+addcreate_m("\n하하하")
+
+
