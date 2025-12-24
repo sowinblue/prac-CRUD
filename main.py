f2c3ee682d0a5c72bac68b61e5d1807a7df61915
sowinblue <249227634+sowinblue@users.noreply.github.com>
2025-12-24 16:40:01 +0900

    인삿말 수정

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 file_name = "to_minamkong.txt"
 
-def create_m(text):
+def newcreate_m(text):
     with open("to_minamkong.txt","w",encoding="utf-8") as f:
-        f.write("안녕하세요 강사님")
-    print("메세지 생성")
+        f.write(text)
+        print("메세지 생성")
+
+
+
+# def addcreate_m(addtext):
+#     with open("to_ninamkong","r",enconding="utf-8") as f:
+#         f.write(addtext)
+#         print("메세지 추가")
+
+
+newcreate_m("안녕하세요 강사님")
